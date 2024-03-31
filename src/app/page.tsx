65faf5e9c15ff2d85d8479c365c4615f43aca0e2
t1onz <buss.vitorsheik@gmail.com>
2024-03-31 05:42:06 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Image from "next/image";
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <Spline scene="https://prod.spline.design/YatQlHq08Yltfiwn/scene.splinecode" />
+  );
+}
